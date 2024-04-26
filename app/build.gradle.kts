7709f33318673c21189ff5edbcdf0c9f18b44fbe
pedro <b_khemari@estin.dz>
2024-04-26 17:16:25 +0100

    changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-firestore:24.11.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.11.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/hackathonapp/MainActivity.kt
+++ app/src/main/java/com/example/hackathonapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.hackathonapp.ViewModels.LoginVM
+import com.example.hackathonapp.screens.HomeScreen
 import com.example.hackathonapp.ui.theme.HackathonAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,12 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HackathonAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+             HomeScreen()
             }
         }
     }
