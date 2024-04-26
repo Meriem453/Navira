d91ec4c8dcdf1b6defbf13c5b0c8edacae17a6a8
HOCINE Mohand Ouidir <hocinidir26@gmail.com>
2024-04-26 20:41:13 +0100

    notification model modification and bottom bar

--- app/src/main/java/com/example/hackathonapp/MainActivity.kt
+++ app/src/main/java/com/example/hackathonapp/MainActivity.kt
@@ -1,58 +1,102 @@
 package com.example.hackathonapp
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
 import com.example.hackathonapp.ViewModels.LoginVM
+import com.example.hackathonapp.navigations.Home
+import com.example.hackathonapp.navigations.Map
+import com.example.hackathonapp.navigations.Navigation
 import com.example.hackathonapp.ui.theme.HackathonAppTheme
+import com.google.accompanist.navigation.material.BottomSheetNavigator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HackathonAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val c= LocalContext.current
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-val vm= hiltViewModel<LoginVM>()
-    vm.login("meriem","123456"){
-        Toast.makeText(c,it.message,Toast.LENGTH_LONG).show()
+fun MainScreen(){
+
+    val navController = rememberNavController()
+
+    val destinationsList = listOf (Home, Map)
+    var selectedIndex by rememberSaveable {
+        mutableIntStateOf(0)
     }
 
-}
+    Scaffold(
+        bottomBar = {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HackathonAppTheme {
-        Greeting("Android")
+            NavigationBar() {
+                destinationsList.forEachIndexed{index, screen ->
+                    NavigationBarItem(
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = screen.icon),
+                                contentDescription = "icon")
+                        },
+                        selected = selectedIndex == index,
+                        onClick = {
+                            selectedIndex = index
+                            navController.navigate(destinationsList[selectedIndex].route){
+                                popUpTo(Home.route)
+                                launchSingleTop = true
+                            }
+                        })
+                }
+            }
+        }
+    ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            Navigation(navController = navController)
+        }
     }
 }
--- app/src/main/java/com/example/hackathonapp/items/Notification_item.kt
+++ app/src/main/java/com/example/hackathonapp/items/Notification_item.kt
@@ -1,5 +1,6 @@
 package com.example.hackathonapp.items
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,10 +13,12 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.hackathonapp.R
 import com.example.hackathonapp.model.Notification
 
 @Composable
@@ -24,15 +27,27 @@ fun NotificationItem(notification: Notification) {
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth(),
-        elevation = CardDefaults.cardElevation(2.dp)
+        elevation = CardDefaults.cardElevation(5.dp),
+        colors = CardDefaults.cardColors(Color.White)
     ) {
-        Row(modifier = Modifier.padding(16.dp)) {
-            Icon(
-                painter = painterResource(id = notification.iconResId), // Use iconResId
-                contentDescription = null,
-                modifier = Modifier.size(40.dp),
-                tint = Color.Transparent
-            )
+        Row(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (notification.state) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_notification_success),
+                    contentDescription = ""
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_notification_warning),
+                    contentDescription = ""
+                )
+            }
+
+
             Spacer(modifier = Modifier.width(16.dp))
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Ship state updated, ")
@@ -42,4 +57,3 @@ fun NotificationItem(notification: Notification) {
         }
     }
 }
-
