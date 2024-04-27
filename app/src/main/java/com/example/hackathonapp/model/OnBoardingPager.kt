087e0197365d2cd1c66b656bf27cce7212985b0e
pedro <b_khemari@estin.dz>
2024-04-27 05:04:32 +0100

    finished getting started

---
+++ app/src/main/java/com/example/hackathonapp/model/OnBoardingPager.kt
@@ -0,0 +1,62 @@
+package com.example.hackathonapp.model
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun OnBoardingPage(
+    modifier: Modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(.8f)
+    ,
+
+    page: Page,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = page.title,
+            fontSize = 36.sp,
+            color = Color.White
+        )
+
+        Text(
+            text = page.description,
+            fontSize = 18.sp,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+    }
+}
+
+@Preview
+@Composable
+private fun OnBoardingPageprev() {
+
+    val page = Page(
+        title = "Welcome Aboard!",
+        description = "Explore the high seas of opportunity with our Cargo Ship Position Management app."
+    )
+    OnBoardingPage(page = page)
+}
+
+
---
+++ app/src/main/java/com/example/hackathonapp/model/page.kt
@@ -0,0 +1,26 @@
+package com.example.hackathonapp.model
+
+import androidx.annotation.DrawableRes
+
+data class Page(
+    val title: String,
+    val description: String,
+
+)
+
+val pages = listOf(
+    Page(
+        title = "Welcome Aboard!",
+        description = "Explore the high seas of opportunity with our Cargo Ship Position Management app."
+    ),
+
+    Page(
+        title = "Coordination",
+        description = "Effortlessly track and manage vessel positions with our user-friendly interface."
+    ),
+
+    Page(
+        title = "Dialogue",
+        description = "Let's set sail! Begin your voyage by creating your account and setting up your fleet."
+    )
+)
